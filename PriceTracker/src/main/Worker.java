85e151267a632ffb07d3b3922b87a48274a979c4
Nicolas Kepper <nicolas.kepper@gmail.com>
2019-04-24 10:48:55 +0200

    Move reading to extra SwingWorker so GUI can refresh during reading

--- PriceTracker/src/main/Worker.java
+++ PriceTracker/src/main/Worker.java
@@ -21,6 +21,7 @@ import javax.swing.JTextField;
 import javax.swing.SwingWorker;
 import javax.swing.border.EmptyBorder;
 
+import structure.AdvancedRobot;
 import structure.Rectangle;
 import structure.ResultDialog;
 
@@ -30,6 +31,8 @@ public class Worker extends ResultDialog {
 	final Rectangle searchArea;
 	final Rectangle searchButton;
 	final Rectangle priceArea;
+	final JTextField txtStep;
+	final JTextField txtResult;
 	
 	private volatile JLabel lblImage;
 	
@@ -50,7 +53,7 @@ public class Worker extends ResultDialog {
 		contentPane.setLayout(new BorderLayout());
 		setContentPane(contentPane);
 		
-		JTextField txtStep = new JTextField();
+		txtStep = new JTextField();
 		txtStep.setEnabled(false);
 		contentPane.add(txtStep, BorderLayout.NORTH);
 		
@@ -58,7 +61,7 @@ public class Worker extends ResultDialog {
 		lblImage.setBackground(Color.BLACK);
 		contentPane.add(lblImage, BorderLayout.CENTER);
 		
-		JTextField txtResult = new JTextField();
+		txtResult = new JTextField();
 		txtResult.setEnabled(false);
 		contentPane.add(txtResult, BorderLayout.SOUTH);
 		
@@ -68,23 +71,19 @@ public class Worker extends ResultDialog {
 			
 		new Screenshotter().execute();
 		
-		try {
-			analyze();
-//			screenshot.run();
-		} catch(AWTException | InterruptedException e) {
-			e.printStackTrace();
-		}
+	
+		new Reader().execute();
 	}
 	
 	
 	private void analyze() throws AWTException, InterruptedException {
-		Robot rob = new Robot();
+		AdvancedRobot rob = new AdvancedRobot();
 		Point searchMid = new Point(searchArea.x + searchArea.width / 2, searchArea.y + searchArea.height / 2);
 		Point buttonMid = new Point(searchButton.x + searchButton.width / 2, searchButton.y + searchButton.height / 2);		
 		for(String item : items) {
-			rob.mouseMove(searchMid.x, searchMid.y);
-			rob.mousePress(InputEvent.BUTTON1_MASK);
-			rob.mouseRelease(InputEvent.BUTTON1_MASK);
+			Point p = MouseInfo.getPointerInfo().getLocation();
+			rob.mouseGlide(p.x, p.y, searchMid.x, searchMid.y, 1000, 100);
+			rob.leftClick();
 			System.out.println("Input for " + item);
 			Thread.sleep(500);
 			rob.mouseMove(buttonMid.x, buttonMid.y);
@@ -103,6 +102,45 @@ public class Worker extends ResultDialog {
 		return null;
 	}
 
+	private class Reader extends SwingWorker<ArrayList<Integer>, String[]> {
+
+		@Override
+		protected ArrayList<Integer> doInBackground() throws Exception {
+			AdvancedRobot rob = new AdvancedRobot();
+			Point searchMid = new Point(searchArea.x + searchArea.width / 2, searchArea.y + searchArea.height / 2);
+			Point buttonMid = new Point(searchButton.x + searchButton.width / 2, searchButton.y + searchButton.height / 2);		
+			String item = items.get(0);
+			for(int i = 0; i < items.size(); item = items.get(++i)) {
+				Point p = MouseInfo.getPointerInfo().getLocation();
+				rob.mouseGlide(p.x, p.y, searchMid.x, searchMid.y, 1000, 100);
+				rob.leftClick();
+				System.out.println("Input for " + item);
+				Thread.sleep(500);
+				rob.mouseMove(buttonMid.x, buttonMid.y);
+				System.out.println("Click");
+				System.out.println("Read data for " + item);
+				Thread.sleep(500);
+				publish(new String[]{(i + 1) + "/" + items.size() + " reading data for " + item});
+			}
+			
+			return new ArrayList<Integer>();
+			
+		}
+
+		@Override
+		protected void process(List<String[]> chunks) {
+			txtStep.setText(chunks.get(chunks.size() - 1)[0]);
+			super.process(chunks);
+		}
+
+		@Override
+		protected void done() {
+			// TODO Auto-generated method stub
+			super.done();
+		}
+		
+	}
+	
 	private class Screenshotter extends SwingWorker<Void, BufferedImage> {
 
 		@Override
